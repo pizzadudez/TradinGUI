d45d9a6adab5c374fa68da68824145a5f428c8b2
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-08-10 21:56:25 +0300

    added material-ui, some custom styling, disabling buttons

--- react_app/src/components/AccountTable/AccountTable.js
+++ react_app/src/components/AccountTable/AccountTable.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchBankers } from '../../actions/postActions'
-//import { fetchBankers } from 'D:\\_Repos\\bankersApp\\react_app\\src\\actions\\postActions';
+import { fetchBankers } from '../../actions/postActions';
+
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
 
 class AccountTable extends Component {
+
   componentWillMount() {
     this.props.fetchBankers();
   }
 
   render() {
-    const test = this.props.bankers.map(banker => (
-      <div key={banker.name}>
+    const StyledList = withStyles({
+      root: {
+        width: 70,
+        size: 12,
+      },
+    })(List);
+
+    const StyledButton = withStyles({
+      root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        width: 70,
+        fontSize: 11,
+        marginBottom: 2,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    })(Button);
+
+    const bankers = this.props.bankers.map(banker => (
+      <StyledButton disabled={banker.realm === 'Blackmoore' ? true : false} >
         {banker.name}
-      </div>
+      </StyledButton>
     ));
 
     return (
-      <div>
-        <div>
-          {this.props.accounts}
-        </div>
-        <div>
-          {test}
-        </div>
-      </div>
+      <StyledList>
+        {bankers}
+      </StyledList>
     ); 
   }
 }
