3c8a67b0424a1bff23e9f543164de4c425bf4934
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-08-20 03:20:03 +0300

    BankerButton selected styling

--- app/src/components/BankerButton.js
+++ app/src/components/BankerButton.js
@@ -10,32 +10,34 @@ const Button = styled.button`
   border: none;
   border-radius: 2px;
   cursor: pointer;
-  background-color: #ffd561;
-  padding: 1px 8px;
+  padding: 1px 4px;
   overflow: hidden;
-  width: 80px;
-  height: 28px;
   margin: 3px;
   z-index: 1;
-  box-shadow: 
-    0 2px 2px 0 rgba(0,0,0,0.14), 
-    0 3px 1px -2px rgba(0,0,0,0.12),
-    0 1px 5px 0 rgba(0,0,0,0.2);
   transition: .3s ease-in-out;
   text-align: center;
   user-select: none; 
   -webkit-tap-highlight-color: transparent;
+  box-shadow: 
+    0 2px 2px 0 rgba(0,0,0,0.14), 
+    0 3px 1px -2px rgba(0,0,0,0.12),
+    0 1px 5px 0 rgba(0,0,0,0.2);
+
+  background-color: ${props => props.selected ? '#7ff238' : '#ffd561'};
+  width: ${props => props.selected ? '180px' : '80px'};
+  height: ${props => props.selected ? '48px' : '28px'};
+  font-size: ${props => props.selected ? '16px' : '13px'}
 
   &:disabled {
-    background-color: #DFDFDF !important;
-    color: #9F9F9F !important;
+    background-color: ${props => props.selected ? '#e4ffd4' : '#DFDFDF'} !important;
+    color: ${props => props.selected ? '#a3a3a3' : '#9f9f9f'} !important;
     pointer-events: none;
     cursor: default;
     box-shadow: none;
   }
   
   &:hover {
-    background: #ffa930;
+    background: ${props => props.selected ? '#38d130' : '#ffa930'};
   } 
 
   &:focus {
@@ -43,7 +45,7 @@ const Button = styled.button`
   }
 
   &:active {
-    background: #db7c32;
+    background: ${props => props.selected ? '#2c9e26' : '#db7c32'};
     transition: .1s ease-in-out;
     transform: translateY(0.5px) translateX(0.5px);
   }
@@ -51,25 +53,32 @@ const Button = styled.button`
 
 class BankerButton extends Component {
   render() {
-    if (!this.props.banker || !this.props.realms) {
+    if (!this.props.banker || this.props.realms.length < 0) {
       return null;
     }
 
-    const buttonText = [
+    const buttonText = this.props.selected
+    ? [
       this.props.banker.account,
       this.props.realms[this.props.banker.realm].code,
+      this.props.banker.name,
+    ].join(' ')
+    : [
+      this.props.realms[this.props.banker.realm].code,
       this.props.banker.bank_num,
+      Math.round(this.props.banker.bank_gold / 1000)
     ].join(' ');
 
     return (
       <Button
         onClick={this.props.onClick}
+        selected={this.props.selected}
         disabled={
           this.props.selected
-          ? this.props.banker.trade_confirmation == 1
+          ? this.props.banker.trade_confirmation === 1
           : this.props.selectedIds.includes(this.props.banker.id)
-          || this.props.banker.trade_confirmation == 1
-        }   
+          || this.props.banker.trade_confirmation === 1
+        }
       >
         {buttonText}
       </Button>
